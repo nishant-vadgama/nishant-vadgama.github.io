7f6dc907135bf82897222120e4f07f9d6f62fa3d
nishant-vadgama <nishantvadgama007@gmail.com>
2022-05-16 08:34:04 -0400

    Added linkedin bedge 2

---
+++ precache-manifest.5f057fdea2942aedf7fb4f317a8d455b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c12acddc1bbd0e08a7f9fbd8e0c013",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6af978eb53287a2ddc8a",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "48332c0b30403c080486",
+    "url": "/static/css/main.72e1d799.chunk.css"
+  },
+  {
+    "revision": "6af978eb53287a2ddc8a",
+    "url": "/static/js/2.f882642a.chunk.js"
+  },
+  {
+    "revision": "48332c0b30403c080486",
+    "url": "/static/js/main.798315eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "73e9d959bae37e6c6dec1720ea463bd5",
+    "url": "/static/media/bg_image.73e9d959.jpg"
+  },
+  {
+    "revision": "1e35964ed67907641874e89279fa3070",
+    "url": "/static/media/profile_image.1e35964e.jpg"
+  }
+]);
